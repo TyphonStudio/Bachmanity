43b07994f530204f46c26adca2cf7bcde2896b3b
djumma <jummatim@gmail.com>
2018-05-04 20:36:57 +0200

    Network messaging
    - identify click on client
    - send a message with it's player's netId to server
    - resend message from server to all clients

---
+++ Assets/Scripts/NetworkPlayer/NetworkMessenger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkMessenger : NetworkBehaviour {
+
+    [Command]
+    public void CmdSendMessageToServer(string message)
+    {
+        Debug.Log("message recieved on server : " + message);
+        RpcSendMessageToClients(message);
+    }
+
+    [ClientRpc]
+    private void RpcSendMessageToClients(string message)
+    {
+        Debug.Log("message recieved on clients : " + message);
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer/NetworkPlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkMessenger))]
+public class NetworkPlayerInput : NetworkBehaviour {
+
+    NetworkMessenger networkMessenger;
+
+    private void Awake()
+    {
+        networkMessenger = GetComponent<NetworkMessenger>();
+    }
+
+    private void Update()
+    {
+        DoInput();
+    }
+
+    private void DoInput()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            networkMessenger.CmdSendMessageToServer("player " + netId + " says hi!");
+        }
+    }
+}
