3d048c23d46b88a0b276b0efce9b748dbcfca6b9
djumma <jummatim@gmail.com>
2018-05-04 18:11:33 +0200

    TextMeshPro import
    - package import
    - simple scene name identifier

---
+++ Assets/Scripts/SceneNameIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNameIdentifier : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI sceneNameText;
+
+	void Start()
+	{
+        sceneNameText.text = SceneManager.GetActiveScene().name;
+	}
+}
