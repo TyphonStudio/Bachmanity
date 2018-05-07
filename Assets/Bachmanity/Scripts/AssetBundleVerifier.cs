54d9b4dd74c02e4880c88a4a5bbe4a62552a0190
djumma <jummatim@gmail.com>
2018-05-07 19:08:29 +0200

    AssetBundle Browser import + verifier

---
+++ Assets/Bachmanity/Scripts/AssetBundleVerifier.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class AssetBundleVerifier : MonoBehaviour {
+
+    [SerializeField]
+    private string bundleName;
+
+    [SerializeField]
+    private string fileName;
+
+    private void Start()
+    {
+        var bundle = AssetBundle.LoadFromFile(Path.Combine("AssetBundles/StandaloneWindows/", bundleName));
+        if (bundle != null)
+        {
+            var prefab = bundle.LoadAsset(fileName);
+
+            if (prefab != null)
+                Debug.Log("asset exists in bundle");
+            else
+                Debug.LogError("no asset with this name found in the bundle");
+        }
+        else
+        {
+            Debug.LogError("no asset bundle found");
+        }
+    }
+}
