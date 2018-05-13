3c446b078ff25537a55c4555dbb76a41cb7646be
djumma <jummatim@gmail.com>
2018-05-13 15:46:54 +0200

    Knight animations import
    - idle
    - hit x 2
    - walk
    - death

--- Assets/Bachmanity/Scripts/Networking/BarrelSpawner.cs
+++ Assets/Bachmanity/Scripts/Networking/BarrelSpawner.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class BarrelSpawner : NetworkSpawner {
+public class BarrelSpawner : NetworkBehaviour {
 
     public static BarrelSpawner Instance { get; private set; }
 
-    protected override void Awake()
+    protected void Awake()
     {
-        base.Awake();
         Instance = this;
+    }
+
+    public GameObject prefab;
+    Vector2 spawnPosition;
+
+    [Server]
+    public void SpawnOnServer(Vector2 spawnPosition)
+    {
+        var obj = Instantiate(prefab, spawnPosition, Quaternion.identity);
+        NetworkServer.Spawn(obj);
     }
 }
---
+++ Assets/Bachmanity/Scripts/Networking/NetworkPlayer/Modules/NetworkSpawningModule.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSpawningModule : NetworkBehaviour
+{
+    public static NetworkSpawningModule Local { get; private set; }
+
+    private Vector2 spawnPosition;
+
+    public override void OnStartLocalPlayer()
+    {
+        Local = this;
+    }
+
+    [Command]
+    public void CmdSpawnBarrelOnServer()
+    {
+        spawnPosition.x = Random.Range(-5.0f, 5.0f);
+        spawnPosition.y = Random.Range(-5.0f, 5.0f);
+
+        BarrelSpawner.Instance.SpawnOnServer(spawnPosition);
+    }
+
+    [Command]
+    public void CmdDestroyWithEffect(NetworkInstanceId objId, NetworkInstanceId initiator)
+    {
+        Debug.Log("destoy command initiate by " + initiator.Value);
+        var obj = NetworkServer.FindLocalObject(objId);
+        RpcPlayEffect(obj.transform.position, initiator);
+        NetworkServer.Destroy(obj);
+    }
+
+    [ClientRpc]
+    void RpcPlayEffect(Vector2 pos, NetworkInstanceId initiator)
+    {
+        Debug.Log("effect initiated by " + initiator.Value);
+
+        // if this client was not the initiator, play effect
+        // otherwise we've already spawned and played it before sending the command
+        if (NetworkPlayer.Local.netId != initiator)
+        {
+            EffectSpawner.Instance.SpawnAndPlay(pos);
+            CameraShaker.Instance.ShakeMainCameraOnce(1.0f, 1.0f);
+        }
+    }
+}
